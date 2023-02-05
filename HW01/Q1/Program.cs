777dadebb9c4dee73bf36e1c82dcc95cad2d6d0c
Kung16845 <111451858+Kung16845@users.noreply.github.com>
2023-02-05 19:13:15 +0700

    Q1

--- HW01/Q1/Program.cs
+++ HW01/Q1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Input your number : ");
+        int num = int.Parse(Console.ReadLine());
+        Console.WriteLine(funtionMath(num));
+    }
+    static double sum = 0;
+    static double funtionMath(int n)
+    {
+        if(Math.Pow(n,2) > 4)
+        {
+            sum = sum + 1+funtionMath(n-(4*Math.Abs(n)/n));
+        }
+        else 
+        {
+            sum = sum+2;
+        }
+        return sum;
+    }
+}
