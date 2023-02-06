b7207fe5e00e621d8f4dafed1c2f392d333862af
Kung16845 <111451858+Kung16845@users.noreply.github.com>
2023-02-07 00:55:17 +0700

    Q3

---
+++ HW01/Q3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Input your number : ");
+        double num = double.Parse(Console.ReadLine());
+        Console.WriteLine(funtionMath(num));
+    }
+    static double sum = 0;
+    static double funtionMath(double x)
+    {
+        if(x > -1 && x <= 0)
+        {
+            sum = sum + Math.Sqrt(1-Math.Pow(x,2));
+        }
+        else if (x > 0 && x <= 1)
+        {
+            sum = sum + (-(Math.Sqrt(1-Math.Pow(x,2)))); 
+        }
+        else if (x <= -1)
+        { 
+            sum = sum + funtionMath(x+2);
+        }
+        else if (x > 1)
+        { 
+            sum = sum + funtionMath(x-2);
+        }
+        return sum;
+    }
+}
